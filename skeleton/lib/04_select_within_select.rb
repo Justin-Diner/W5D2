fbd187da38974691f1b9e73dfd19bde81b67181e
smuushi <mishih780@gmail.com>
2023-02-07 16:16:55 -0500

    asdf

--- skeleton/lib/04_select_within_select.rb
+++ skeleton/lib/04_select_within_select.rb
@@ -81,6 +81,21 @@ def neighbors_of_certain_b_countries
   # List the name and continent of countries in the continents containing
   # 'Belize', 'Belgium'.
   execute(<<-SQL)
+    SELECT
+      name, continent
+    FROM
+      countries
+    WHERE
+      continent IN 
+          (SELECT
+            continent
+          FROM 
+            countries
+          WHERE
+            name in ('Belize', 'Belgium'));
+
+
+
   SQL
 end
 
@@ -88,6 +103,31 @@ def population_constraint
   # Which country has a population that is more than Canada but less than
   # Poland? Show the name and the population.
   execute(<<-SQL)
+
+    SELECT
+      name, population
+    FROM
+      countries
+    WHERE
+      population >
+      (
+        SELECT
+          population
+        FROM 
+          countries
+        WHERE
+          name LIKE 'Canada'
+      ) 
+      
+      AND population < (
+              SELECT
+                population
+              FROM 
+                countries
+              WHERE
+              name LIKE 'Poland'  
+            );
+
   SQL
 end
 
